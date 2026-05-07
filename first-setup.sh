e991f15f911f9a954727500b0ec874c3e64c6aa9
alfbaro-mac-pro <alfbaro-mac-pro@192.168.0.111>
2026-05-07 11:12:56 -0500

    Add config alfred

--- first-setup.sh
+++ first-setup.sh
@@ -29,7 +29,7 @@ install_packages() {
     brew install "${packages[@]}"
 
     log "Installing GUI applications..."
-    local casks=(iterm2 sublime-text sublime-merge proxyman)
+    local casks=(iterm2 sublime-text sublime-merge proxyman alfred)
     brew install --cask "${casks[@]}"
 }
 
